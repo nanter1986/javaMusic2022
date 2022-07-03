ee8d875652e6caa93fa601c2b0a000fd0daf9927
nanter <nanter1986@gmail.com>
2022-07-03 07:06:04 +0300

    first

---
+++ src/MelodyMaker.java
@@ -0,0 +1,20 @@
+public class MelodyMaker {
+    public void makeMelodyBar(int energyLevel){
+        int volume;
+        int baseDuration;
+        int octave;
+        switch (energyLevel){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            default:
+        }
+    }
+}
